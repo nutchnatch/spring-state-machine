633d91e71b2fe896fcaffe8b1a0d33cbf376ee53
nivaldomonteiro <nushnivaldo@gmail.com>
2020-10-31 15:47:55 +0000

    Added spring state  machine example.

---
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscssm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState state;
+    private BigDecimal amount;
+}
